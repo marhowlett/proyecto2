69add05e6d67860969fe1872e2c97a91d10ff620
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-07-09 18:49:32 -0500

    base de datos

--- pulga.sql
+++ pulga.sql
@@ -2,7 +2,7 @@
 SQLyog Ultimate v11.28 (64 bit)
 MySQL - 5.7.17-log : Database - pulga
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -15,6 +15,57 @@ MySQL - 5.7.17-log : Database - pulga
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`pulga` /*!40100 DEFAULT CHARACTER SET utf32 */;
 
 USE `pulga`;
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `usuario` varchar(40) NOT NULL,
+  `contraseña` varchar(40) DEFAULT NULL,
+  `nombre` varchar(40) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`usuario`,`contraseña`,`nombre`,`apellidos`) values ('jramonell','02c0f8926441b227d8ff9b0a4824cbec','jaime','ramonell'),('jrr','202cb962ac59075b964b07152d234b70','jaime','jaime'),('mar','202cb962ac59075b964b07152d234b70','mar','ramonell'),('marimar','202cb962ac59075b964b07152d234b70','Marimar','Ramonell'),('vero','202cb962ac59075b964b07152d234b70','vero','cuellar');
+
+/*Table structure for table `tipo_producto` */
+
+DROP TABLE IF EXISTS `tipo_producto`;
+
+CREATE TABLE `tipo_producto` (
+  `tipo_producto` varchar(40) NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`tipo_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+/*Data for the table `tipo_producto` */
+
+insert  into `tipo_producto`(`tipo_producto`,`descripcion`) values ('01','bolsas'),('02','aretes'),('03','collares');
+
+
+/*Table structure for table `inventario` */
+
+DROP TABLE IF EXISTS `inventario`;
+
+CREATE TABLE `inventario` (
+  `id_producto` varchar(10) NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `tipo_producto` varchar(40) DEFAULT NULL,
+  `cantidad` int(10) DEFAULT NULL,
+  `precio` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `inventario_tipoproducto` (`tipo_producto`),
+  CONSTRAINT `inventario_tipoproducto` FOREIGN KEY (`tipo_producto`) REFERENCES `tipo_producto` (`tipo_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+/*Data for the table `inventario` */
+
+insert  into `inventario`(`id_producto`,`descripcion`,`tipo_producto`,`cantidad`,`precio`) values ('01','01','01',22,50),('02','02','03',10,2),('A00-2','aretes dorados','02',10,20),('B00-3','bolsa roja','01',10,180),('C00-4','Collar azul','03',10,20);
+
+
 
 /*Table structure for table `compra` */
 
@@ -37,54 +88,6 @@ CREATE TABLE `compra` (
 
 insert  into `compra`(`folio_compra`,`fecha_compra`,`id_producto`,`cantidad`,`usuario`) values (1,'2018-07-07 13:43:43','A00-2',2,'marimar'),(6,'2018-07-09 04:19:35','B00-3',6,'mar'),(7,'2018-07-09 04:19:35','B00-3',6,'mar'),(8,'2018-07-09 04:19:35','B00-3',6,'mar'),(9,'2018-07-09 04:27:54','01',2,'mar'),(10,'2018-07-09 04:28:55','A00-2',3,'mar'),(11,'2018-07-09 04:29:13','01',5,'mar'),(12,'2018-07-09 04:29:13','01',5,'mar'),(13,'2018-07-09 04:29:13','01',5,'mar'),(14,'2018-07-09 04:29:13','01',5,'mar'),(15,'2018-07-09 04:35:03','01',2,'mar'),(16,'2018-07-09 04:37:18','01',2,'mar'),(17,'2018-07-09 04:37:18','01',2,'mar'),(18,'2018-07-09 04:38:04','01',2,'mar'),(19,'2018-07-09 04:39:38','01',6,'mar'),(20,'2018-07-09 04:39:38','01',6,'mar');
 
-/*Table structure for table `inventario` */
-
-DROP TABLE IF EXISTS `inventario`;
-
-CREATE TABLE `inventario` (
-  `id_producto` varchar(10) NOT NULL,
-  `descripcion` varchar(50) DEFAULT NULL,
-  `tipo_producto` varchar(40) DEFAULT NULL,
-  `cantidad` int(10) DEFAULT NULL,
-  `precio` int(10) DEFAULT NULL,
-  PRIMARY KEY (`id_producto`),
-  KEY `inventario_tipoproducto` (`tipo_producto`),
-  CONSTRAINT `inventario_tipoproducto` FOREIGN KEY (`tipo_producto`) REFERENCES `tipo_producto` (`tipo_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
-/*Data for the table `inventario` */
-
-insert  into `inventario`(`id_producto`,`descripcion`,`tipo_producto`,`cantidad`,`precio`) values ('01','01','01',22,50),('02','02','03',10,2),('A00-2','aretes dorados','02',10,20),('B00-3','bolsa roja','01',10,180),('C00-4','Collar azul','03',10,20);
-
-/*Table structure for table `tipo_producto` */
-
-DROP TABLE IF EXISTS `tipo_producto`;
-
-CREATE TABLE `tipo_producto` (
-  `tipo_producto` varchar(40) NOT NULL,
-  `descripcion` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`tipo_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
-/*Data for the table `tipo_producto` */
-
-insert  into `tipo_producto`(`tipo_producto`,`descripcion`) values ('01','bolsas'),('02','aretes'),('03','collares');
-
-/*Table structure for table `usuario` */
-
-DROP TABLE IF EXISTS `usuario`;
-
-CREATE TABLE `usuario` (
-  `usuario` varchar(40) NOT NULL,
-  `contraseña` varchar(40) DEFAULT NULL,
-  `nombre` varchar(40) DEFAULT NULL,
-  `apellidos` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
-/*Data for the table `usuario` */
-
-insert  into `usuario`(`usuario`,`contraseña`,`nombre`,`apellidos`) values ('jramonell','02c0f8926441b227d8ff9b0a4824cbec','jaime','ramonell'),('jrr','202cb962ac59075b964b07152d234b70','jaime','jaime'),('mar','202cb962ac59075b964b07152d234b70','mar','ramonell'),('marimar','202cb962ac59075b964b07152d234b70','Marimar','Ramonell'),('vero','202cb962ac59075b964b07152d234b70','vero','cuellar');
 
 /*Table structure for table `venta` */
 
