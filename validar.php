a30297b2370d6cb6c09106088cd915c29e84e8fb
juandax10 <juanda.pro10@gmail.com>
2018-07-09 20:01:40 -0500

    Validacion del lado servidor

--- validar.php
+++ validar.php
@@ -3,7 +3,7 @@
 session_start();
 require 'conexionBD/connection.php';
 
-$tbl_name = "usuario";
+$ban = 0;
 
 // Destruir todas las variables de sesi�n.
 $_SESSION = array();
@@ -11,22 +11,29 @@ if (!isset($_SESSION["username"])){
   $_SESSION["username"] = "";
 
 }
-$ban = 0;
-$user=$_POST["username"];  //lee usuario del formulario
-$clave1=$_POST["password"];  //lee clave del formulario
-$clave = md5($clave1);
-$sql1 = "Select * from usuario where usuario='$user' and contraseña='$clave'";// saca el total masculino
-echo ($sql1);
-$conexion->real_query($sql1);
-$resultado = $conexion->use_result();
-while ($fila = $resultado->fetch_assoc()) {
-    $ban = 1;
-}
-$resultado->close();
-if ($ban==1){
-    $_SESSION["username"]=$user;
-    header("Location: index2.php");
+//validacion del lado del servidor (campos no vacios)
+if (empty($_POST["username"]) || empty($_POST["password"])) {
+  header("Location:loginin.php");
+  exit();
 }else{
-    header("Location: loginin.php");
+  $user=$_POST["username"];  //lee usuario del formulario
+  $clave1=$_POST["password"];  //lee clave del formulario
+  $clave = md5($clave1);
+  $sql1 = "Select * from usuario where usuario='$user' and contraseña='$clave'";// saca el total masculino
+  echo ($sql1);
+  $conexion->real_query($sql1);
+  $resultado = $conexion->use_result();
+  while ($fila = $resultado->fetch_assoc()) {
+      $ban = 1;
+  }
+  $resultado->close();
+  if ($ban==1){
+      $_SESSION["username"]=$user;
+      header("Location: index2.php");
+      exit();
+  }else{
+      header("Location: loginin.php");
+      exit();
+  }
 }
  ?>
