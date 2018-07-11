ed2efaf7394d6ca4a1a6ac5a491ebf5606646a88
juandax10 <juanda.pro10@gmail.com>
2018-07-10 20:47:30 -0500

    ordenando lista

--- lista_inventario.php
+++ lista_inventario.php
@@ -23,6 +23,9 @@ location.href="index.php";
 
     <link rel="shortcut icon" type="image/x-icon" href="css/134027.png" />
     <link rel="stylesheet" type="text/css" href="css/prueba.css" />
+    <script src="js/fontawesome-all.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"
+          integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
 <link
    rel="stylesheet"
    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
@@ -41,6 +44,7 @@ $sql1="SELECT * FROM inventario";
    $consultaUsuarios = $conexion->use_result();
 
 ?>
+<body>
 <header>
 
     <div class="text-center">
@@ -71,6 +75,12 @@ $sql1="SELECT * FROM inventario";
 </header>
 
 <br><br>
+<div class="container">
+  <div class="row">
+    <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Buscar producto.." name="nombre_producto" id="tags">
+</div>
+  </div>
 
 <div class="table-responsive">
    <table class="table table-striped align">
@@ -100,3 +110,43 @@ $sql1="SELECT * FROM inventario";
        </tbody>
    </table>
 </div>
+</div>
+</body>
+<script type="text/javascript">
+
+$(document).ready(function () {
+  $("#tags").on("keyup", function () {
+                var value = $(this).val().toUpperCase();
+                removeHighlighting($("table tr em"));
+                $("table tr").each(function (index) {
+                    if (index !== 0) {
+                        $row = $(this);
+                        var $tdElement = $row.find("td:nth-child(2)");
+                        var id = $tdElement.text();
+                        var matchedIndex = id.indexOf(value);//returns the position of the first occurrence of a specified value in a string.
+                        if (matchedIndex != 0) {
+                            $row.hide();
+                        }
+                        else {
+                            addHighlighting($tdElement, value);
+                            $row.show();
+                        }
+                    }
+                });
+            });
+            function removeHighlighting(highlightedElements) {
+                highlightedElements.each(function () {
+                    var element = $(this);
+                    element.replaceWith(element.html());
+                })
+            }
+
+            function addHighlighting(element, textToHighlight) {
+                var text = element.text().toUpperCase();
+                var highlightedText = '<em>' + textToHighlight + '</em>';
+                var newText = text.replace(textToHighlight, highlightedText);
+                element.html(newText);
+            }
+          });
+</script>
+</html>
